406fc4388a0e5d3da45cf0cc230810aad6a0b18f
MuhammadAbyaz <58655276+MuhammadAbyaz@users.noreply.github.com>
2023-12-02 14:04:55 +0000

    added more linked list functions

--- linked_list.py
+++ linked_list.py
@@ -10,6 +10,19 @@ class LinkedList():
   head = None
   tail = None
 
+  def __len__(self):
+    return self.size()
+
+  def size(self):
+    current = self.head
+    count = 0
+
+    while current.next != None:
+      count += 1
+      current = current.next
+    count += 1
+    return count
+
   def append(self, value):
     new_node = Node(value)
     if self.head is None:
@@ -19,6 +32,59 @@ class LinkedList():
       self.tail.next = new_node
       self.tail = new_node
 
+  def remove(self, index):
+    if index == 0:
+      self.head = self.head.next
+    current = Node()
+    current = self.head
+    for _ in range(1, index):
+      current = current.next
+    temp = current.next.next
+    current.next = temp
+
+  def pop(self):
+    current = self.head
+    while current.next.next != None:
+      current = current.next
+    self.tail = current
+    self.tail.next = None
+
+  def insert(self, index, value):
+    new_node = Node(value)
+    if index == 0:
+      temp = self.head
+      self.head = new_node
+      self.head.next = temp
+    current = self.head
+    for _ in range(1, index):
+      current = current.next
+    temp = current.next
+    current.next = new_node
+    current.next.next = temp
+
+  def get_value(self, index):
+    current = self.head
+
+    for _ in range(0, index):
+      current = current.next
+    return current.value
+
+  def reverse(self):
+    prevoius = None
+    current = self.head
+    while current.next != None:
+      next = current.next
+      if current == self.head:
+        current.next = None
+        prevoius = current
+        current = next
+      else:
+        current.next = prevoius
+        prevoius = current
+        current = next
+    self.tail.next = prevoius
+    self.head, self.tail = self.tail, self.head
+
   def to_string(self):
     current = self.head
     print("[", end="")
@@ -26,15 +92,3 @@ class LinkedList():
       print(f"{current.value} ,", end="")
       current = current.next
     print(f"{current.value}]", end="")
-
-  def remove(self, index):
-    pass
-
-  def pop(self):
-    pass
-
-  def insert(self, index, value):
-    pass
-
-  def get_value(self, index):
-    pass
--- main.py
+++ main.py
@@ -7,3 +7,10 @@ my_list.append(4)
 my_list.append(5)
 
 my_list.to_string()
+my_list = LinkedList()
+my_list.append(2)
+my_list.append(3)
+my_list.append(4)
+my_list.append(5)
+
+my_list.to_string()
